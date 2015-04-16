5e4fa2c218487ae395768d27a7ee38074d0b1f93
Annie Pennell <annie@anniepennell.com>
2015-04-15 17:18:27 -0700

    checks that email is unique

--- models/user.js
+++ models/user.js
@@ -50,14 +50,23 @@ module.exports = function(sequelize, DataTypes) {
       createSecure: function(email, password, username, name, location) {
         if(password.length < 6) {
           throw new Error("Password too short");
+        } else {
+          var _this = this;
+          return this.count( {where: {email: email}})
+          .then(function(userCount) {
+            if (userCount >= 1) {
+              throw new Error("Accout already exists for this email.");
+            } else {
+              return _this.create({
+                email: email,
+                passwordDigest: _this.encryptPassword(password),
+                username: username,
+                name: name,
+                location: location
+              });
+            }
+          })
         }
-        return this.create({
-          email: email,
-          passwordDigest: this.encryptPassword(password),
-          username: username,
-          name: name,
-          location: location
-        });
       },
       authenticate: function(email, password) {
         // find a user in the DB
