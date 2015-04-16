bd31d5fcca47e59c612c9a673be3698de66b93fc
Annie Pennell <annie@anniepennell.com>
2015-04-16 15:20:37 -0700

    adding production db

--- models/index.js
+++ models/index.js
@@ -6,6 +6,7 @@ var Sequelize = require("sequelize");
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || "development";
 var config    = require(__dirname + '/../config/config.json')[env];
+
 // NEEDED FOR HEROKU ///////////
 if(config.use_env_variable){
   var db_info = process.env[config.use_env_variable].match(/([^:]+):\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
